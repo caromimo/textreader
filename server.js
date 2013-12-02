50e21739524daca7bf5f6400803d1e5be03c5f98
Mike Williamson <mike@korora.ca>
2013-12-02 04:31:25 -0500

    Refactor to separate app from server

--- server.js
+++ server.js
@@ -1,9 +1,6 @@
 var http = require('http');
-var express = require ('express');
-var app = express();
-http.createServer(app).listen(2222, function() { 
+var app = require('./src/app');
+
+http.createServer(app).listen(2222, function() {
   console.log("S.V.P. veuillez patienter... la vie est belle :)");
-});
-app.get('/', function (request, response) {
-  response.send("Bonjour!");
 });
