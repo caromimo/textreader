c5e45ac889e6512b13a45ddb26c4a4381ca5b136
Mike Williamson <mike@korora.ca>
2013-12-03 08:57:10 -0500

    Add first passing test.

--- spec/server_spec.js
+++ spec/server_spec.js
@@ -1,10 +1,29 @@
+var request = require('request');
 var app = require('../src/app');
+var server;
 
 describe('textreader', function(){
 
-    it('respond to /', function(){
-      expect(true).toBe(true);
+  beforeEach(function(done){
+    server = app.listen(3333, function (err, result) {
+      if (err) {
+        done(err);
+      } else {
+        done();
+      }
     });
+  });
+
+  afterEach(function(){
+    server.close();
+  });
+
+  it('responds to /', function(done){
+      request.get("http://localhost:3333", function(error, response, body){
+        expect(body).toEqual('Bonjour!');
+        done();
+      });
+  });
 
 });
 
