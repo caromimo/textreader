52571ee5c5fcc5d577f526366fc40554855ebf83
Mike Williamson <mike@korora.ca>
2013-12-03 12:29:10 -0500

    Correct environment test to deal with async.

--- spec/environment_spec.js
+++ spec/environment_spec.js
@@ -1,22 +1,27 @@
-var exec = require('child_process').exec;
 
 describe('the environment', function(){
 
-  it('can find espeak', function(){
+  it('can find espeak', function(done){
+    var exec = require('child_process').exec;
     exec('espeak -h', function(error, stdout, stderr){
-      expect(error).toBe(null);
+      expect(stdout).toNotEqual('');
+      done();
     });
   });
 
-  it('can find mbrola', function(){
+  it('can find mbrola', function(done){
+    var exec = require('child_process').exec;
     exec('mbrola -h', function(error, stdout, stderr){
-      expect(error).toBe(null);
+      expect(stdout).toNotEqual('');
+      done();
     });
   });
 
-  it('can find lame', function(){
+  it('can find lame', function(done){
+    var exec = require('child_process').exec;
     exec('lame -h', function(error, stdout, stderr){
-      expect(error).toBe(null);
+      expect(stdout).toNotEqual('');
+      done();
     });
   });
 
