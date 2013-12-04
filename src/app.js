f01cdc54297137b5fc2ea406566a523ee90e951d
Mike Williamson <mike@korora.ca>
2013-12-04 09:38:00 -0500

    Remove console.log statement.

--- src/app.js
+++ src/app.js
@@ -6,10 +6,8 @@ app.get('/vocalization/fr/homme', function (request, response) {
   response.header('Content-Type', 'audio/mpeg');
   response.header('Content-Disposition', 'inline; filename=test.mp3');
   var exec = require('child_process').exec;
-
-  //console.log(request.query["texte"]);
-
   var text = request.query.texte;
+
   // Below espeak takes text and transform it to an audio format (pho), than
   // mbrola transform the pho file to raw audio and provides the voice and
   // finally, lame transforms the raw audio to mp3.
