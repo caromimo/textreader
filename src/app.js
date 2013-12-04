22085cf0ec5c1aba51b263634151a77f8397ccdb
Mike Williamson <mike@korora.ca>
2013-12-04 14:19:25 -0500

    Use tmpfile instead of hardcoding filename.

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+var tmp = require('tmp');
 var express = require('express');
 var app = express();
 app.use(express.bodyParser());
@@ -8,12 +9,16 @@ app.get('/vocalization/fr/homme', function (request, response) {
   var exec = require('child_process').exec;
   var text = request.query.texte;
 
-  // Below espeak takes text and transform it to an audio format (pho), than
-  // mbrola transform the pho file to raw audio and provides the voice and
-  // finally, lame transforms the raw audio to mp3.
-  var command = 'espeak -v mb-fr1 -q --pho -a 15 -p 50 -s 130 "' + text + '" | mbrola /usr/share/mbrola/fr1/fr1 - - | lame -r -m m -b 24 -s 16 - test.mp3';
-  exec(command, function (error, stdout, stderr) {
-    response.sendfile('./test.mp3');
+
+  tmp.tmpName(function _tempNameGenerated(err, tempfile) {
+    if (err) throw err;
+    // Below espeak takes text and transform it to an audio format (pho), than
+    // mbrola transform the pho file to raw audio and provides the voice and
+    // finally, lame transforms the raw audio to mp3.
+    var command = 'espeak -v mb-fr1 -q --pho -a 15 -p 50 -s 130 "' + text + '" | mbrola /usr/share/mbrola/fr1/fr1 - - | lame -r -m m -b 24 -s 16 - ' + tempfile;
+    exec(command, function (error, stdout, stderr) {
+      response.sendfile(tempfile);
+    });
   });
 });
 
