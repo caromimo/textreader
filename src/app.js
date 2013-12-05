3f99616dcd99dca7422af9f63d32ef9b433b765a
Mike Williamson <mike@korora.ca>
2013-12-05 10:50:53 -0500

    Removed mbrola.

--- src/app.js
+++ src/app.js
@@ -12,10 +12,10 @@ app.get('/vocalization/fr/homme', function (request, response) {
 
   tmp.tmpName(function _tempNameGenerated(err, tempfile) {
     if (err) throw err;
-    // Below espeak takes text and transform it to an audio format (pho), than
-    // mbrola transform the pho file to raw audio and provides the voice and
-    // finally, lame transforms the raw audio to mp3.
-    var command = 'espeak -v mb-fr1 -q --pho -a 15 -p 50 -s 130 "' + text + '" | mbrola /usr/share/mbrola/fr1/fr1 - - | lame -r -m m -b 24 -s 16 - ' + tempfile;
+    // Below espeak takes text and transform it to wav
+    // using the mbrola voice (prefixed with mb-) then
+    // we use lame transforms the raw audio to mp3.
+    var command = 'espeak -v mb-fr1 -w ' + tempfile + ' -a 15 -p 50 -s 130 "' + text + '" | lame -r -m m -b 24 -s 16 ' + tempfile + ' ' + tempfile;
     exec(command, function (error, stdout, stderr) {
       response.sendfile(tempfile);
     });
