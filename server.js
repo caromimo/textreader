4b896fb7e93288d805f7a7042bed946d6c8dce03
Mike Williamson <mike@korora.ca>
2013-12-06 09:04:41 -0500

    Update server startup message.

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ var http = require('http');
 var app = require('./src/app');
 
 http.createServer(app).listen(2222, function() {
-  console.log("S.V.P. veuillez patienter... la vie est belle :)");
+  console.log("Server is listening on port 2222");
 });
