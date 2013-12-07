ac1f1975555750693cfc6fac35828e1ce1f040df
Caroline Mimeault <carolinemimeault@gmail.com>
2013-12-07 22:48:08 +0000

    Translated the message in French.

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ var http = require('http');
 var app = require('./src/app');
 
 http.createServer(app).listen(2222, function() {
-  console.log("Server is listening on port 2222");
+  console.log("Le serveur est activé et à l'écoute sur le port 2222");
 });
