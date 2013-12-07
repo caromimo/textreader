ed603763c8293ed3605ecef08f1d01498d37c995
Caroline Mimeault <carolinemimeault@gmail.com>
2013-12-07 22:49:16 +0000

    Made voice selection dynamic.

--- src/app.js
+++ src/app.js
@@ -3,14 +3,14 @@ var express = require('express');
 var app = express();
 app.use(express.bodyParser());
 
-app.get('/vocalization/fr/homme', function (request, response) {
+app.get('/vocalization/:voix', function (request, response) {
   response.header('Content-Type', 'audio/mpeg');
   response.header('Content-Disposition', 'inline; filename=test.mp3');
   var spawn = require('child_process').spawn;
 
   tmp.tmpName(function _tempNameGenerated(err, espeakTmpfile) {
     if (err) throw err;
-    var espeak = spawn('espeak', ['-vmb-fr1', '-w' + espeakTmpfile , '-s130', request.query.texte]);
+    var espeak = spawn('espeak', ['-vmb-' + request.params.voix , '-w' + espeakTmpfile , '-s130', request.query.texte]);
 
     espeak.on('exit', function(exitCode){
       tmp.tmpName(function _tempNameGenerated(err, lameTmpfile) {
