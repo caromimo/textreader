d002cc61057e40744944733acea7eee5aa169577
Mike Williamson <mike@korora.ca>
2013-12-11 15:32:17 +0000

    Replace fs.statSync with fs.stat.

--- src/app.js
+++ src/app.js
@@ -26,13 +26,14 @@ app.get('/vocalization/:voix', function (request, response) {
 
           lame.on('exit', function(exitCode){
             if(err) throw err;
-            var stat = fs.statSync(lameTmpfile);
             response.header('Content-Type', 'audio/mpeg');
-            response.header('Content-Length', stat.size - 1);
-            // There is a click that appears at the beginning of the audio.
-            // we start reading 1 byte in to avoid it:
-            var readstream = fs.createReadStream(lameTmpfile, {start: 1});
-            readstream.pipe(response);
+            fs.stat(lameTmpfile, function (err, stats) {
+              response.header('Content-Length', stats.size - 1);
+              // There is a click that appears at the beginning of the audio.
+              // we start reading 1 byte in to avoid it:
+              var readstream = fs.createReadStream(lameTmpfile, {start: 1});
+              readstream.pipe(response);
+            });
           });
 
           lame.stderr.on('data', function(data){
